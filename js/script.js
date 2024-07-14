65ce965f5734714de01b4968ab5c1c2e7df40c6d
KevinSonda <kevinsonda@gmail.com>
2024-07-14 18:18:09 +0200

    [UPD]verifico che il numero non sia incluso nell'array

--- js/script.js
+++ js/script.js
@@ -9,6 +9,13 @@ function createArrayNumbers() {
 
     //verifico se la lunghezza dell'array e minore di 5. Se e vera la condizione genero il numero e lo pusho nell'array
     while (numbers.length < 5) {
-        
+        // genero il numero con la funzione defiita sopra
+        let num = generateRandomNumber();
+
+        //verifico che il numero non sia incluso nell'array
+        if (!numbers.includes(num)) {
+            //pusho il numero nell'array
+            numbers.push(num);
+        }
     }
 }
